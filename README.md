d20013db77adb726322a98e9588e1f2d0a892fb5
duannd <ngdduan@gmail.com>
2021-12-17 10:38:20 +0700

    Add Spring Web MVC Servlet module.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SpringBoot Technology Stack
+
+# SpringBoot Web Servlet example
+
+1. Reference Doc [Web Servlet](./springboot-web-servlet/README.md)
---
+++ springboot-web-servlet/src/main/java/com/duannd/spring/boot/WebServletApplication.java
@@ -0,0 +1,13 @@
+package com.duannd.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServletApplication.class, args);
+    }
+
+}
